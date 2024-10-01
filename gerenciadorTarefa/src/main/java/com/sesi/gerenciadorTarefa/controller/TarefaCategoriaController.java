0c61c8b41997971bf4613448838ae31efbc920ba
Hectro2414 <hectorlaucher6@gmail.com>
2024-10-01 10:01:29 -0300

    Finalizando projeto

--- gerenciadorTarefa/src/main/java/com/sesi/gerenciadorTarefa/controller/TarefaCategoriaController.java
+++ gerenciadorTarefa/src/main/java/com/sesi/gerenciadorTarefa/controller/TarefaCategoriaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sesi.gerenciadorTarefa.model.TarefaCategoria;
 import com.sesi.gerenciadorTarefa.repository.TarefaCategoriaRepository;
@@ -24,21 +25,46 @@ public class TarefaCategoriaController {
 		return"listarTarefaCategoria";
 	}
 	
+	@PostMapping("/salvarCategoria")
+	public String salvarCategoria(TarefaCategoria categoria) {
+		tarefaCategoriaRepository.save(categoria);
+		return "redirect:/listarCategoria";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	@GetMapping("/editarCategoria/{id}")
 	public String editarCategoria(@PathVariable("id") int id, Model modelo) {
 		Optional<TarefaCategoria> categoriaOpt = tarefaCategoriaRepository.findById(id);
 		
 		
 		if (categoriaOpt.isPresent()) {
-			modelo.addAttribute("categroia", categoriaOpt.get());
+			modelo.addAttribute("categoria", categoriaOpt.get());
 			return"formularioTarefaCategoria";
 		} else {
 			return "redirect:/listarTarefaCategoria";
 		}
 	}
 	
+	
+	
+	@GetMapping("/excluirCategoria/{id}")
+	public String excluirCategoria(@PathVariable("id") int id) {
+		tarefaCategoriaRepository.deleteById(id);
+		return "redirect:/listarCategoria";
+	}
+	
+	
+	
 	@GetMapping("/formularioCategoria")
-	public String mostrarFormulario() {
+	public String mostrarFormulario( Model modelo) {
+		modelo.addAttribute("categoria", new TarefaCategoria());
 		
 		return"formularioTarefaCategoria";
 	}
---
+++ gerenciadorTarefa/src/main/java/com/sesi/gerenciadorTarefa/repository/UsuarioRepository.java
@@ -0,0 +1,5 @@
+package com.sesi.gerenciadorTarefa.repository;
+
+public interface UsuarioRepository {
+
+}
